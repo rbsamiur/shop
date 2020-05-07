325a843196b1b3c6ea38de56b43f56afb0ad04a9
rbsamiur <64774912+rbsamiur@users.noreply.github.com>
2020-05-07 18:53:38 +0600

    Update searchCart.js

--- src/mixin/searchCart.js
+++ src/mixin/searchCart.js
@@ -1,7 +1,7 @@
-export default{
+
   computed:{
     filteredCartItems:function(){
-      return this.mycart.filter((item)=>{
+      return this.$store.state.mycart.filter((item)=>{
         return item.name.match(this.search);
       })
     }
